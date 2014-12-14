18895cd510637b2a654fea77cfcac2c9ff8c2f36
LincecumHao <only520you2002@gmail.com>
2014-12-15 00:12:32 +0800

    首頁樣版設計

---
+++ ZKprj/CommunityDDB/WebContent/res/script/initMap.js
@@ -0,0 +1,14 @@
+google.maps.event.addDomListener(window, 'load', initialize);
+var MAP;
+function initialize() {
+	
+	var mapOptions = {
+		center : new google.maps.LatLng(-34.397, 150.644),
+		zoom : 8,
+		mapTypeId : google.maps.MapTypeId.ROADMAP
+	};
+	var mapId = zk.Widget.$('$map_canvas').$n().id;
+	console.log(mapId);
+	this.MAP = new google.maps.Map(document.getElementById(mapId), mapOptions);
+	console.log('done');
+}
